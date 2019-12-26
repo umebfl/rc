08c432dca15de39154336ac46e4eac43a458ec73
huanganqi <1079255145@qq.com>
2019-12-26 17:47:43 +0800

    背景图片

---
+++ style.js
@@ -0,0 +1,12 @@
+import {
+  StyleSheet,
+} from 'react-native'
+
+
+export default StyleSheet.create({
+    main: {
+        backgroundColor: '#333',
+        display: 'flex',
+        flex: 1,
+    },
+})
