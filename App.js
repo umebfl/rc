8d542f35a4317fc465dc602d7375f3ab175ff517
huanganqi <1079255145@qq.com>
2019-12-26 18:59:31 +0800

    去背景

--- App.js
+++ App.js
@@ -4,25 +4,84 @@ import {
   SafeAreaView,
   View,
   Text,
-  ImageBackground,
   ScrollView,
+  TabBarIOS,
 } from 'react-native'
 
 import style from './style'
 
-const bg = require('./pic/bg.jpg')
-
 const App: () => React$Node = () => {
     return (
-        <ImageBackground style={style.bg} source={bg}>
-            <SafeAreaView>
-                <ScrollView>
-                    <Text style={style.footer}>Engine: Hermes</Text>
-                </ScrollView>
-            </SafeAreaView>
-        </ImageBackground>
+        <SafeAreaView>
+            <ScrollView>
+                <View style={style.block}>
+                    <Text style={style.block_title}>交易系统</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>规则</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>交易策略</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>理念</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>交易流程</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>典籍</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>元素</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>职业体系</Text>
+                </View>
+                <View style={style.block}>
+                    <Text style={style.block_title}>年度结算</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 
 export default App
+
+
+// const bg = require('./pic/bg.jpg')
+// <ImageBackground style={style.bg} source={bg}></ImageBackground>
+
+
+// <SafeAreaView>
+//     <ScrollView>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>交易系统</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>规则</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>交易策略</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>理念</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>交易流程</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>典籍</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>元素</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>职业体系</Text>
+//         </View>
+//         <View style={style.block}>
+//             <Text style={style.block_title}>年度结算</Text>
+//         </View>
+//     </ScrollView>
+// </SafeAreaView>
--- style.js
+++ style.js
@@ -12,4 +12,19 @@ export default StyleSheet.create({
         display: 'flex',
         flex: 1,
     },
+
+    // 分层
+    block: {
+        backgroundColor: 'white',
+        marginBottom: 20,
+        paddingLeft: 4,
+        paddingRight: 4,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 6,
+    },
+    block_title: {
+        color: 'black',
+        fontSize: 16,
+    },
 })
