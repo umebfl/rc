915f4d3ccf2e0552fb2fb3d577e1774c642192ec
huanganqi <1079255145@qq.com>
2019-12-30 12:26:02 +0800

    手工筛选品种

--- system.js
+++ system.js
@@ -35,18 +35,20 @@ export default class System extends Component {
 
 
     componentDidMount() {
-        AsyncStorage.getItem('data', (e, rv) => {
-            if(rv) {
-                console.log('flow', '完成存储数据加载')
-                this.setState({
-                    data: JSON.parse(rv),
-                })
+        // AsyncStorage.getItem('data', (e, rv) => {
+        //     if(rv) {
+        //         console.log('flow', '完成存储数据加载')
+        //         this.setState({
+        //             data: JSON.parse(rv),
+        //         })
+        //
+        //         this.fetch_current_data()
+        //     } else {
+        //         this.fetch_current_data()
+        //     }
+        // })
 
-                this.fetch_current_data()
-            } else {
-                this.fetch_current_data()
-            }
-        })
+        this.fetch_current_data()
 
         setInterval(() => {
             this.fetch_current_data()
@@ -78,13 +80,16 @@ export default class System extends Component {
 
                     info.pop()
 
-                    const current_bond = (parseInt(info[8]) * breed_item.unit * breed_item.rate).toFixed(0)
+                    const price = parseInt(info[8]).toFixed(0)
+
+                    const current_bond = (price * breed_item.unit * breed_item.rate).toFixed(0)
 
                     ft_log = [
                         ...ft_log,
                         {
                             ...breed_item,
                             info,
+                            price,
                             // 当前一手保证金
                             current_bond,
                             // 当前持仓总金额
@@ -96,7 +101,7 @@ export default class System extends Component {
                 this.setState({
                     data: ft_log,
                 })
-                console.log('flow', '完成当前数据的请求')
+                console.log('flow', '完成当前数据的请求', ft_log)
             } else {
                 Toast.fail('请求接口数据异常！')
             }
@@ -108,9 +113,9 @@ export default class System extends Component {
 
     handle_reload_log() {
 
-        AsyncStorage.clear()
+        // AsyncStorage.clear()
 
-        const data = this.props.context.state.breed
+        const data = this.state.data
         let finish_count = 0
         let ft_log = []
 
@@ -134,6 +139,9 @@ export default class System extends Component {
                         // 只要前2.5年600天
                         R.takeLast(600)
                     )(JSON.parse(request.responseText))
+
+                    // 杠杆
+                    const real_rate = (1 / item.rate).toFixed(0)
 
                     // 主力连续 当前价格
                     const main_price = all_day[all_day.length - 1][4]
@@ -182,6 +190,7 @@ export default class System extends Component {
                         {
                             ...item,
                             all_day,
+                            real_rate,
                             price_max,
                             spread_max_rate,
                             price_min,
@@ -200,14 +209,14 @@ export default class System extends Component {
                             data: ft_log,
                         })
 
-                        try {
-                            AsyncStorage.setItem('data', JSON.stringify(ft_log))
-                        } catch (error) {
-                            Toast.fail('存储数据失败！')
-                        }
+                        // try {
+                        //     AsyncStorage.setItem('data', JSON.stringify(ft_log))
+                        // } catch (error) {
+                        //     Toast.fail('存储数据失败！')
+                        // }
 
-                        console.log('flow', '完成日志数据的请求并写入存储', ft_log)
-                        this.fetch_current_data()
+                        console.log('flow', '完成全部日志数据的请求', ft_log)
+                        this.handle_reload_log_month()
                     }
 
                 } else {
@@ -216,6 +225,132 @@ export default class System extends Component {
             }
 
             request.open('GET', `http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesDailyKLine?symbol=${code}0`)
+            request.send()
+        }
+    }
+
+    handle_reload_log_month() {
+
+        const data = this.state.data
+        let finish_count = 0
+        let ft_log = []
+
+        for (var i = 0; i < data.length; i++) {
+            const code = data[i].code
+            const month = data[i].month
+            const request = new XMLHttpRequest()
+            const item = data[i]
+
+            request.onreadystatechange = (e) => {
+                if (request.readyState !== 4) {
+                    return
+                }
+
+                if (request.status === 200) {
+                    const all_day = R.compose(
+                        R.map(
+                            R.addIndex(R.map)(
+                                (v, k) => k === 0 ? v : parseInt(v)
+                            ),
+                        ),
+                        // 只要前2.5年600天
+                        // R.takeLast(600)
+                    )(JSON.parse(request.responseText))
+
+                    // 当前价格
+                    const price = item.price
+
+                    // 最高价
+                    const price_max = parseInt(R.reduce((a, b) => b[4] !== 0 && a > b[4] ? a : b[4], price_max)(all_day))
+
+                    // 距离最高 百分比
+                    const spread_max_rate = ((price_max - price) / price * 100).toFixed(1)
+
+                    // 最低价
+                    const price_min = parseInt(R.reduce((a, b) => b[4] !== 0 && a > b[4] ? b[4] : a, price_max)(all_day))
+
+                    // 距离最高 百分比
+                    const spread_min_rate = ((price - price_min) / price * 100).toFixed(1)
+
+                    // 价格分段
+
+                    // 各价格段数量统计
+
+                    // 当前价位状态
+                    const price_state = ((price - price_min) / (price_max - price_min)).toFixed(2)
+
+                    const price_state_cn = R.cond([
+                        [
+                            v => v < 0.3,
+                            R.always('低'),
+                        ],
+                        [
+                            v => v > 0.7,
+                            R.always('高'),
+                        ],
+                        [
+                            R.T,
+                            R.always('中'),
+                        ],
+                    ])(price_state)
+
+                    // 最高最低波幅 附加保证金比例
+                    // const max_to_min_rate = ((price_max - price_min) / price_min).toFixed(1)
+                    const max_to_min_rate = ((price_max - price_min) / price_min * (1 / item.rate)).toFixed(1)
+
+                    // 一周前价格
+                    const before_a_week_price = all_day[all_day.length - 5 * 4][4]
+                    // 一周波幅
+                    const before_a_week_rate = ((price - before_a_week_price) / before_a_week_price * 100).toFixed(1)
+                    // 一周盈利率
+                    const before_a_week_real_rate = (Math.abs(price - before_a_week_price) / before_a_week_price * item.real_rate * 100).toFixed(1)
+
+                    ft_log = [
+                        ...ft_log,
+                        {
+                            ...item,
+                            month_data: {
+                                all_day,
+                                price_max,
+                                spread_max_rate,
+                                price_min,
+                                spread_min_rate,
+                                max_to_min_rate,
+                                price,
+                                price_state,
+                                price_state_cn,
+
+                                before_a_week_price,
+                                before_a_week_rate,
+                                before_a_week_real_rate,
+
+                            },
+                        }
+                    ]
+
+                    finish_count++
+
+                    if(finish_count === data.length) {
+                        this.setState({
+                            data: ft_log,
+                        })
+
+                        // try {
+                        //     AsyncStorage.setItem('data', JSON.stringify(ft_log))
+                        // } catch (error) {
+                        //     Toast.fail('存储数据失败！')
+                        // }
+
+                        console.log('flow', '完成指定月合约日志数据的请求', ft_log)
+                        // this.fetch_current_data()
+                    }
+
+                } else {
+                    Toast.fail('请求接口数据异常！')
+                }
+            }
+
+            request.open('GET', `http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesDailyKLine?symbol=${code}${month}`)
             request.send()
         }
     }
@@ -259,7 +394,11 @@ export default class System extends Component {
                                             <View style={{flexDirection: 'row', flowWrap: 'wrap', fontSize: 10}}>
                                                 <Text style={{width: 50}}>{v.name}{R.takeLast(2)(v.month)}</Text>
                                                 {/* 当前价  */}
-                                                <Text style={{width: 50, textAlign: 'right'}}>{v.info ? parseInt(v.info[8]).toFixed(0) : '-'}</Text>
+                                                <Text style={{width: 50, textAlign: 'right'}}>{v.info ? v.price : '-'}</Text>
+                                                {/* 一周前价格 */}
+                                                <Text style={{width: 90, textAlign: 'right'}}>{v.month_data ? `${v.month_data.before_a_week_price}(${v.month_data.before_a_week_rate})` : '-'}</Text>
+                                                {/* 一周盈利率 */}
+                                                <Text style={{width: 40, textAlign: 'right'}}>{v.month_data ? `${v.month_data.before_a_week_real_rate}` : '-'}</Text>
                                                 {/* 价格状态  */}
                                                 <Text style={{width: 50, textAlign: 'right'}}>{v.price_state_cn ? `${v.price_state_cn}${v.price_state}` : '-'}</Text>
                                                 {/* 极端波幅  */}
@@ -269,7 +408,7 @@ export default class System extends Component {
                                                 {/* 持仓资金  */}
                                                 <Text style={{width: 40, textAlign: 'right', color: '#999'}}>{Math.round(v.current_hold_amount_totle / 100000000) + '亿'}</Text>
                                                 {/* 保证金比例  */}
-                                                <Text style={{width: 20, textAlign: 'right', color: '#999'}}>{(1 / v.rate).toFixed(0)}</Text>
+                                                <Text style={{width: 20, textAlign: 'right', color: '#999'}}>{v.real_rate ? v.real_rate : '-'}</Text>
                                                 {/* 一手保证金  */}
                                                 <Text style={{width: 50, textAlign: 'right', color: '#999'}}>{v.current_bond ? v.current_bond : '-'}</Text>
                                                 <Text style={{width: 90, textAlign: 'right', color: '#999'}}>{v.price_max ? `${v.price_max}-${v.spread_max_rate}` : '-'}</Text>
@@ -280,10 +419,16 @@ export default class System extends Component {
                                 ),
                                 R.sort(
                                     (a ,b) => {
+                                        if(a.month_data) {
+                                            return b.month_data.before_a_week_real_rate - a.month_data.before_a_week_real_rate
+                                        } else {
+                                            return true
+                                        }
+
                                         if(a.max_to_min_rate) {
-                                            return b.price_state - a.price_state
+                                            // return b.price_state - a.price_state
                                             // 波幅排序
-                                            // return b.max_to_min_rate - a.max_to_min_rate
+                                            return b.max_to_min_rate - a.max_to_min_rate
                                         } else {
                                             return true
                                         }
